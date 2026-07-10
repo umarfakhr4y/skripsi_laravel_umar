8c5c27e5e89ae4281926a41f338a03cf5c9e78a6
Umar <umarfakhr4y@gmail.com>
2026-07-10 21:15:29 +0700

    [ADD] nambahin field data sudah isi laporan harian boolean

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,6 +18,7 @@ class UserResource extends JsonResource
 
         $sudahAbsen = false;
         $dataAbsenHariIni = null;
+        $sudahIsiLaporan = false;
 
         if ($this->role === 'peserta' && $this->peserta) {
             $absen = \App\Models\Absensi::where('peserta_magang_id', $this->peserta->id)
@@ -28,6 +29,14 @@ class UserResource extends JsonResource
                 $sudahAbsen = true;
                 $dataAbsenHariIni = $absen;
             }
+
+            $laporan = \App\Models\LaporanHarian::where('peserta_magang_id', $this->peserta->id)
+                            ->whereDate('created_at', \Carbon\Carbon::now()->toDateString())
+                            ->first();
+            
+            if ($laporan) {
+                $sudahIsiLaporan = true;
+            }
         }
 
         return [
@@ -35,6 +44,7 @@ class UserResource extends JsonResource
             'email'      => $this->email,
             'role'       => $this->role,
             'sudah_absen'=> $sudahAbsen,
+            'sudah_isi_laporan' => $sudahIsiLaporan,
             'absen_hari_ini' => $dataAbsenHariIni,
             'data'       => $profileData,
         ];
